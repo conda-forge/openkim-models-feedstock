02d03ae4085c7a8d4c228156689de339bbc75610
Ryan S. Elliott <relliott@umn.edu>
2021-02-11 11:53:29 -0600

    Remove patch and use kim-api cmake args instead

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 tar Jxvf ${PKG_NAME}-${PKG_VERSION//./-}.txz
 cd ${PKG_NAME}-${PKG_VERSION//./-}
-patch -u -p1 -i ${RECIPE_DIR}/cmake.patch
-cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
+cmake . \
+      -DKIM_API_CMAKE_CXX_COMPILER=${CXX_FOR_BUILD} \
+      -DKIM_API_CMAKE_C_COMPILER=${CC_FOR_BUILD} \
+      -DKIM_API_CMAKE_Fortran_COMPILER=${FC} \
+      -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers \
+      -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models \
+      -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
 make install
